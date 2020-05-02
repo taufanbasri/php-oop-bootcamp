14dbbb459ce7bce92a284288b72e9e59ed3b9689
taufanbasri <taufanprasetyobasri@gmail.com>
2020-05-02 10:40:11 +0700

    Classes

---
+++ classes/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+class Task {
+    public $description;
+    public $completed = false;
+
+    public function __construct($description) {
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+}
+
+$task = new Task('Learn OOP');
+$task->complete();
+
+var_dump($task);
